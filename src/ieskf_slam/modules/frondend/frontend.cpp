cc2d77a7c7942d8b29edd9b21654f1e5a968681d
Meng Kai <kai.meng2@desaysv.com>
2025-03-28 17:27:02 +0800

    fix bugs

--- src/ieskf_slam/modules/frondend/frontend.cpp
+++ src/ieskf_slam/modules/frondend/frontend.cpp
@@ -49,6 +49,9 @@ namespace IESKFSlam
     }
     void FrontEnd::addPointCloud(const PointCloud&pointcloud){
         pointcloud_deque.push_back(pointcloud);
+        pcl::transformPointCloud(*pointcloud_deque.back().cloud_ptr,
+                                 *pointcloud_deque.back().cloud_ptr,
+                                 compositeTransform(extrin_r, extrin_t).cast<float>());
     }
     bool FrontEnd::track(){
         MeasureGroup mg;
@@ -72,9 +75,6 @@ namespace IESKFSlam
     }
     const PCLPointCloud& FrontEnd::readCurrentPointCloud(){
         return *filter_point_cloud_ptr;
-    }
-    const PCLPointCloud &FrontEnd::readCurrentLocalMap(){
-        return *map_ptr->getLocalMap();
     }
     bool FrontEnd::syncMeasureGroup(MeasureGroup&mg){
         mg.imus.clear();
